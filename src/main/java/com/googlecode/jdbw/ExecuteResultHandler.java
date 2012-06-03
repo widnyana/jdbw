a4f08a08e1f65a00e50b71268eadc42d35070a08
Martin Berglund <mabe02@gmail.com>
2012-06-03 12:35:52 +0900

    JavaDoc

--- src/main/java/com/googlecode/jdbw/ExecuteResultHandler.java
+++ src/main/java/com/googlecode/jdbw/ExecuteResultHandler.java
@@ -27,19 +27,65 @@ import java.util.List;
  */
 public interface ExecuteResultHandler {
 
+    /**
+     * This method is called just before the query is sent to the database, 
+     * setting the property on the Statement which limits the number of rows
+     * we want back. It's up to the driver implementation and the server if this
+     * limit will be honored or not (I think)
+     * @return Number of rows to fetch from the server, 0 means no limit
+     */
     int getMaxRowsToFetch();
 
+    /**
+     * This callback is invoked when a new result set is read from the server,
+     * if the result set contains any rows they will appear in subsequent calls
+     * to the nextRow(Object[] row) callback.
+     * @param columnNames List of all column names
+     * @param columnTypes List of all column types (see java.sql.Types)
+     */
     void onResultSet(List<String> columnNames, List<Integer> columnTypes);
 
+    /**
+     * The callback is called once for every row returned by a result set. The
+     * row belongs to the result set defined by the last call to onResultSet.
+     * @param row All the values of the row
+     * @return true if you want to read more rows, false if you want to close 
+     * the result set and skip remaining rows
+     */
     boolean nextRow(Object[] row);
-
+    
+    /**
+     * This method is called in a multi result set query after one result set
+     * have been read and another one is coming. Expect another call to 
+     * onResultSet as well, for the next result set.
+     * @return true if you want to read the next result set, false if you want
+     * to skip remaining result sets and close the statement
+     */
     boolean nextResultSet();
 
+    /**
+     * Callback called for queries that updated rows
+     * @param updateCount Number of rows affected by the query, as reported by
+     * the server
+     */
     void onUpdateCount(int updateCount);
 
+    /**
+     * When a key has been generated on the server by the query, this callback
+     * is called with that value
+     * @param object Generated key value
+     */
     void onGeneratedKey(Object object);
 
+    /**
+     * Callback called for every SQLWarning the server sent for the query
+     * @param warning SQLWarning from the server
+     */
     void onWarning(SQLWarning warning);
 
+    /**
+     * This method is called when all processing of the query is done and the
+     * statement is about to be closed.
+     */
     void onDone();
 }
