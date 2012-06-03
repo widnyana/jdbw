832535b4797a5833a54085e578a40c6d56728fc4
Martin Berglund <mabe02@gmail.com>
2012-06-03 12:54:54 +0900

    JavaDoc

--- src/main/java/com/googlecode/jdbw/SQLExecutor.java
+++ src/main/java/com/googlecode/jdbw/SQLExecutor.java
@@ -22,14 +22,42 @@ import java.sql.SQLException;
 import java.util.List;
 
 /**
- *
+ * The SQLExecutor is the object sending the query to the remote database server
+ * and using a callback interface to process the result. This query will take care
+ * of creating Statements and ResultSets and make sure to close everything when done.
+ * This object is most frequently used through the sub-interface DatabaseTransaction
+ * or AutoExecutor.
+ * @see DatabaseTransaction
+ * @see AutoExecutor
  * @author mabe02
  */
 public interface SQLExecutor {
 
+    /**
+     * Sends a query to the database and handles any results through a callback interface
+     * @param handler Callback interface to use for any results of the query
+     * @param SQL SQL code to send to the database server, use ? for parameter substitution
+     * @param parameters List of parameters to insert into the query, must be one for every ? used
+     * @throws SQLException 
+     */
     void execute(ExecuteResultHandler handler, String SQL, Object... parameters) throws SQLException;
 
+    /**
+     * Executes a list of queries as one batch on the remote database server
+     * @param handler Callback interface to use for any results of the query
+     * @param batchedSQL List of SQL to send to the remote server
+     * @throws SQLException 
+     */
     void batchWrite(BatchUpdateHandler handler, List<String> batchedSQL) throws SQLException;
 
+    /**
+     * Executes a batch query where the SQL is structurally the same but parameters 
+     * are different. Usefull for inserting, updating or deleting multiple rows
+     * from the same table.
+     * @param handler Callback interface to use for any results of the query
+     * @param SQL SQL to use for all queries, use ? for the parameter substitution
+     * @param parameters List of object arrays, where one array equals one query sent to the server
+     * @throws SQLException 
+     */
     void batchWrite(BatchUpdateHandler handler, String SQL, List<Object[]> parameters) throws SQLException;
 }
