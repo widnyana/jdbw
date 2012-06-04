c22964786b93817637b9676876a50816bf49c7d8
Martin Berglund <mabe02@gmail.com>
2012-06-04 13:46:23 +0900

    LGPL

--- src/main/java/com/googlecode/jdbw/util/SQLWorker.java
+++ src/main/java/com/googlecode/jdbw/util/SQLWorker.java
@@ -1,6 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of jdbw (http://code.google.com/p/jdbw/).
+ * 
+ * jdbw is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) 2009-2012 mabe02
  */
 
 package com.googlecode.jdbw.util;
@@ -8,14 +22,12 @@ package com.googlecode.jdbw.util;
 import com.googlecode.jdbw.SQLExecutor;
 import java.math.BigInteger;
 import java.sql.SQLException;
-import java.sql.SQLWarning;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
 /**
  *
- * @author mberglun
+ * @author mabe02
  */
 public class SQLWorker
 {
