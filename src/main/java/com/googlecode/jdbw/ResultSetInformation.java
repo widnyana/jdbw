54954a4c683386b64cc7592f5258b7f5f3418dc1
Martin Berglund <mabe02@gmail.com>
2014-07-25 22:16:59 +0900

    New interface for keeping result set information in one place

---
+++ src/main/java/com/googlecode/jdbw/ResultSetInformation.java
@@ -0,0 +1,46 @@
+package com.googlecode.jdbw;
+
+import java.sql.ResultSetMetaData;
+
+/**
+ * This interface contains extracted values from ResultSetMetaData describing a ResultSet that JDBW has received. It can
+ * be safely examined without requiring to catch SQLExceptions.
+ * @author Martin Berglund
+ */
+public interface ResultSetInformation {
+    /**
+     * Returns the underlying ResultSetMetaData object that was used to construct this ResultSetInformation.
+     * @return ResultSetMetaData that was used to construct this ResultSetInformation
+     */
+    ResultSetMetaData getResultSetMetaData();
+
+    /**
+     * For queries with only one ResultSet, this always returns 0. For queries with multiple ResultSets, this will be 0
+     * for the first, 1 for the second and so on.
+     * @return Index of the ResultSet in a multi-ResultSet context
+     */
+    int getResultSetIndex();
+
+    /**
+     * Returns how many columns the result set has.
+     * @return How many columns has the ResultSet
+     */
+    int getNumberOfColumns();
+
+    /**
+     * Returns the name (or label) of the columnIndex:th column (first column has index 0)
+     * @param columnIndex Index of the column to get the name for
+     * @return Name of the column with index columnIndex.
+     * @throws java.lang.ArrayIndexOutOfBoundsException If columnIndex was out of bounds
+     */
+    String getColumnLabel(int columnIndex) throws ArrayIndexOutOfBoundsException;
+
+    /**
+     * Returns the type code, as specified by java.sql.Types, of the column with index columnIndex (first column has
+     * index 0).
+     * @param columnIndex Index of the column to get the type for
+     * @return Constant from java.sql.Types representing the column's data type
+     * @throws ArrayIndexOutOfBoundsException If columnIndex was out of bounds
+     */
+    int getColumnSQLType(int columnIndex) throws ArrayIndexOutOfBoundsException;
+}
